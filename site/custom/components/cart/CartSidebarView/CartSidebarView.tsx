9220f7e1d5400c092dc4d90f50a59652141e32ad
Gabriel Orofino <g.orofino@protonmail.com>
2023-09-04 12:16:31 -0300

    Copied the SidebarLayout component and updated the import in CartSidebarView.

--- site/custom/components/cart/CartSidebarView/CartSidebarView.tsx
+++ site/custom/components/cart/CartSidebarView/CartSidebarView.tsx
@@ -8,7 +8,7 @@ import { useUI } from '@components/ui/context'
 import { Bag, Cross, Check } from '@components/icons'
 import useCart from '@framework/cart/use-cart'
 import usePrice from '@framework/product/use-price'
-import SidebarLayout from '@components/common/SidebarLayout'
+import SidebarLayout from '@custom/common/SidebarLayout'
 
 const CartSidebarView: FC = () => {
   const { closeSidebar, setSidebarView } = useUI()
