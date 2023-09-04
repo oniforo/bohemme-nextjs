5141e1fa4146c2768ab94fc9183b21b558bdec05
Gabriel Orofino <g.orofino@protonmail.com>
2023-09-04 01:25:11 -0300

    Changed fill to white.

--- site/components/icons/Info.tsx
+++ site/components/icons/Info.tsx
@@ -4,7 +4,7 @@ const Info = ({ ...props }) => {
       viewBox="0 0 24 24"
       width="24"
       height="24"
-      stroke="currentColor"
+      stroke="white"
       strokeWidth="1.5"
       strokeLinecap="round"
       strokeLinejoin="round"
@@ -13,8 +13,8 @@ const Info = ({ ...props }) => {
       {...props}
     >
       <circle cx="12" cy="12" r="10" fill="transparent" />
-      <path d="M12 8v4" stroke="currentColor" />
-      <path d="M12 16h.01" stroke="currentColor" />
+      <path d="M12 8v4" stroke="white" />
+      <path d="M12 16h.01" stroke="white" />
     </svg>
   )
 }
