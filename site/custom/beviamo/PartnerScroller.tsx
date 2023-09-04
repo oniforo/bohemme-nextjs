f7ad17bf4d687c89140b4f4ea001ff938ee83dc7
Gabriel Orofino <g.orofino@protonmail.com>
2023-09-03 22:42:51 -0300

    Added key component for children in iterator.

--- site/custom/beviamo/PartnerScroller.tsx
+++ site/custom/beviamo/PartnerScroller.tsx
@@ -15,7 +15,7 @@ const PartnerGroup = () => {
         <div className='flex w-min animate-slide group-hover:pause'>
             {
                 partners.map(logo => (
-                    <div className='mx-4 w-24 md:w-32 hover:scale-110 cursor-pointer'>
+                    <div key={logo} className='mx-4 w-24 md:w-32 hover:scale-110 cursor-pointer'>
                         <Image src={'/partners/'.concat(logo)} width={300} height={300} alt='' />
                     </div>
             ))
